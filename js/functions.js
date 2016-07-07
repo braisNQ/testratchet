30e7cd6d62dca383f1cf7214640af2d020648057
CARRION ANSIAS Brais <bcarrion@emea.msad.sopra>
2016-07-07 12:05:23 +0200

    Added counter controlled by cookies

---
+++ js/functions.js
@@ -0,0 +1,41 @@
+function showCount(){
+    var count = getCookie("count");
+
+    if (count == "")
+        count = 0;
+
+    document.getElementById("p_count").innerHTML = count;
+}
+
+function incrementCount(increment){
+    var count = getCookie("count");
+
+    if (count == "")
+        count = increment;
+    else
+        count = parseInt(count) + increment;
+
+    setCookie("count" , count, 999);
+
+    document.getElementById("p_count").innerHTML = count;
+}
+
+function decrementCount(decrement){
+    var count = getCookie("count");
+
+    if (count == "")
+        count = 0;
+    else if (parseInt(count) <= 0)
+        count = 0;
+    else
+        count = parseInt(count) - decrement;
+
+    setCookie("count" , count, 999);
+
+    document.getElementById("p_count").innerHTML = count;
+}
+
+function resetCount(){
+    setCookie("count", 0, 999);
+    showCount();
+}
